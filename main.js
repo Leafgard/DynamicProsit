fe2ab757a01eb4727be7afe092ac43ba1d172676
Yann Seget <dev@leafgard.fr>
2019-05-17 16:10:23 +0200

    Removed DevTools on start

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function createWindow() {
   })
   mainWindow.loadFile('views/app.html')
   // DevTools
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     mainWindow = null
   })
